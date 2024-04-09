99431d751b07d6acb20154abab4752e292335c5e
wamuni <boshi.pan@outlook.com>
2024-04-09 10:20:53 +0100

    1. add APICaller in Target rather than under the same DI file 2. git init should start from project rather than DI directory 3. url and completion method as parameter 4. check if url exist first 5. declare http task with provided url 6. handle data return back from api

---
+++ APICaller.swift
@@ -0,0 +1,43 @@
+//
+//  APICaller.swift
+//  APIKit
+//
+//  Created by 潘博石 on 09/04/2024.
+//
+
+import Foundation
+
+public class APICaller {
+    static let shared = APICaller()
+    
+    public func fetchCoursesName(url: URL, completion: @escaping (([String]) -> Void)) {
+        
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
+            completion([])
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                completion([])
+                return
+            }
+            
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [[String: String]] else {
+                    completion([])
+                    return
+                }
+                
+                let names = json.compactMap({$0["name"]})
+                completion(names)
+                
+            } catch {
+                completion([])
+            }
+            
+        }
+        
+        task.resume()
+    }
+}
---
+++ APIKit.h
@@ -0,0 +1,18 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by 潘博石 on 09/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
